9f35f9aa5e0f4dbfcf6a7fcfcd1019518c03f428
Tango-King <wyfgxl526@163.com>
2020-01-15 23:20:48 +0800

    add regist

---
+++ src/main/java/life/tangoking/community/controller/IndexController.java
@@ -0,0 +1,20 @@
+package life.tangoking.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(name="name", defaultValue = "World!") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "hello";
+//    }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
